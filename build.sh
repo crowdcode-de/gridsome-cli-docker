91dff30042e79f8824a5e4edf79bc4e074f07b90
Ingo Düppe <ingo.dueppe@crowdcode.io>
2020-05-17 17:32:56 +0200

    fix configuration

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Build a docker image containing node, npm, and vue cli
-#Set the required vue cli version
+# Set the required gridsome cli version
 VUE_CLI_VERSION='4.3.1'
 GRIDSOME_CLI_VERSION='0.7.14'
 docker build -t "crowdcode/gridsome-cli:${GRIDSOME_CLI_VERSION}" --build-arg VUE_CLI_VERSION=${VUE_CLI_VERSION} --build-arg GRIDSOME_CLI_VERSION=${GRIDSOME_CLI_VERSION} .
